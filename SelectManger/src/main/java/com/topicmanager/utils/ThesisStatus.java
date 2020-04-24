a7694a6f7a985a4ca4ae8c52a54a4baeab11a31b
Dusksnow <599286017@qq.com>
2020-04-24 21:40:27 +0800

    -

---
+++ SelectManger/src/main/java/com/topicmanager/utils/ThesisStatus.java
@@ -0,0 +1,7 @@
+package com.topicmanager.utils;
+
+public class ThesisStatus {
+    public static String PENDING = "待审核";
+    public static String SUCESS = "确认通过";
+    public static String REJECT = "不通过";
+}
