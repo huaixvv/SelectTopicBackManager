dd1d2825a9cf2be76a4dba0e3bf67c8213b2d928
Dusksnow <599286017@qq.com>
2020-04-27 12:21:48 +0800

    选题功能完善

---
+++ SelectManger/src/main/java/com/topicmanager/service/OrderInfoService.java
@@ -0,0 +1,4 @@
+package com.topicmanager.service;
+
+public class OrderInfoService {
+}
--- SelectManger/src/main/java/com/topicmanager/utils/ThesisStatus.java
+++ SelectManger/src/main/java/com/topicmanager/utils/ThesisStatus.java
@@ -3,5 +3,5 @@ package com.topicmanager.utils;
 public class ThesisStatus {
     public static String PENDING = "待审核";
     public static String SUCESS = "确认通过";
-    public static String REJECT = "不通过";
+    public static String REJECT = "未通过";
 }
